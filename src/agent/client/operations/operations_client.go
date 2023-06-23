b1c80aa9f8a969d55287e95d1c6305faa608dae9
hanzezhenalex <hanalex109@gmail.com>
2023-06-23 06:41:25 +0000

    swagger gen

--- src/agent/client/operations/operations_client.go
+++ src/agent/client/operations/operations_client.go
@@ -32,6 +32,10 @@ type ClientOption func(*runtime.ClientOperation)
 type ClientService interface {
 	GetV1ConnectionList(params *GetV1ConnectionListParams, opts ...ClientOption) (*GetV1ConnectionListOK, error)
 
+	PostLogin(params *PostLoginParams, opts ...ClientOption) (*PostLoginOK, error)
+
+	PostV1AuthUserCreate(params *PostV1AuthUserCreateParams, opts ...ClientOption) (*PostV1AuthUserCreateOK, error)
+
 	SetTransport(transport runtime.ClientTransport)
 }
 
@@ -70,6 +74,82 @@ func (a *Client) GetV1ConnectionList(params *GetV1ConnectionListParams, opts ...
 	// unexpected success response
 	// safeguard: normally, absent a default response, unknown success responses return an error above: so this is a codegen issue
 	msg := fmt.Sprintf("unexpected success response for GetV1ConnectionList: API contract not enforced by server. Client expected to get an error, but got: %T", result)
+	panic(msg)
+}
+
+/*
+  PostLogin login in the socks system.
+*/
+func (a *Client) PostLogin(params *PostLoginParams, opts ...ClientOption) (*PostLoginOK, error) {
+	// TODO: Validate the params before sending
+	if params == nil {
+		params = NewPostLoginParams()
+	}
+	op := &runtime.ClientOperation{
+		ID:                 "PostLogin",
+		Method:             "POST",
+		PathPattern:        "/login",
+		ProducesMediaTypes: []string{"application/json"},
+		ConsumesMediaTypes: []string{"application/json"},
+		Schemes:            []string{"https"},
+		Params:             params,
+		Reader:             &PostLoginReader{formats: a.formats},
+		Context:            params.Context,
+		Client:             params.HTTPClient,
+	}
+	for _, opt := range opts {
+		opt(op)
+	}
+
+	result, err := a.transport.Submit(op)
+	if err != nil {
+		return nil, err
+	}
+	success, ok := result.(*PostLoginOK)
+	if ok {
+		return success, nil
+	}
+	// unexpected success response
+	// safeguard: normally, absent a default response, unknown success responses return an error above: so this is a codegen issue
+	msg := fmt.Sprintf("unexpected success response for PostLogin: API contract not enforced by server. Client expected to get an error, but got: %T", result)
+	panic(msg)
+}
+
+/*
+  PostV1AuthUserCreate create new user.
+*/
+func (a *Client) PostV1AuthUserCreate(params *PostV1AuthUserCreateParams, opts ...ClientOption) (*PostV1AuthUserCreateOK, error) {
+	// TODO: Validate the params before sending
+	if params == nil {
+		params = NewPostV1AuthUserCreateParams()
+	}
+	op := &runtime.ClientOperation{
+		ID:                 "PostV1AuthUserCreate",
+		Method:             "POST",
+		PathPattern:        "/v1/auth/user/create",
+		ProducesMediaTypes: []string{"application/json"},
+		ConsumesMediaTypes: []string{"application/json"},
+		Schemes:            []string{"https"},
+		Params:             params,
+		Reader:             &PostV1AuthUserCreateReader{formats: a.formats},
+		Context:            params.Context,
+		Client:             params.HTTPClient,
+	}
+	for _, opt := range opts {
+		opt(op)
+	}
+
+	result, err := a.transport.Submit(op)
+	if err != nil {
+		return nil, err
+	}
+	success, ok := result.(*PostV1AuthUserCreateOK)
+	if ok {
+		return success, nil
+	}
+	// unexpected success response
+	// safeguard: normally, absent a default response, unknown success responses return an error above: so this is a codegen issue
+	msg := fmt.Sprintf("unexpected success response for PostV1AuthUserCreate: API contract not enforced by server. Client expected to get an error, but got: %T", result)
 	panic(msg)
 }
 
